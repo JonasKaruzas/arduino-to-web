219d2b0c4925fc377410a40039047a7e5e8d8a71
Jonas Karuzas <jonas.karuzas@gmail.com>
2021-02-18 21:43:42 +0200

    arduino to node works

---
+++ app.js
@@ -0,0 +1,20 @@
+var SerialPort = require("serialport");
+
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+    delimeter: '\r\n'
+});
+
+var port = new SerialPort('COM9', {
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
+
+parser.on('data', function(data) {
+    console.log(data);
+})
