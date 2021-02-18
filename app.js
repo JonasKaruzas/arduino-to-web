c61dc7e4b091cc204bc5880c9ccb0e168cae0df5
Jonas Karuzas <jonas.karuzas@gmail.com>
2021-02-18 23:00:38 +0200

    viskas veikia

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+
+var index = fs.readFileSync('index.html');
+
 var SerialPort = require("serialport");
 
 const parsers = SerialPort.parsers;
@@ -15,6 +20,31 @@ var port = new SerialPort('COM9', {
 
 port.pipe(parser);
 
+
+
+var app = http.createServer(function(req,res) {
+    
+    res.writeHead(200, {'Content_Type':'text/html'});
+    res.end(index);
+    
+});
+
+var io = require('socket.io')(app);
+
+
+io.on('connection', function(socket) {
+    console.log('Node is listeningm on connection');
+
+});
+
 parser.on('data', function(data) {
     console.log(data);
-})
+
+    io.emit('data', data);
+});
+
+
+
+app.listen(3000, () => {
+    console.log('go to localhost:3000')
+});
